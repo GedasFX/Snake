5df2cb113445040c69897024f9d74adec8057f79
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-09-21 13:15:18 +0300

    make panel singlebuffered

--- Snake/DoubleBufferedPanel.cs
+++ Snake/DoubleBufferedPanel.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Snake.Controls
 {
-    public class DoubleBufferedPanel: Panel
+    public sealed class DoubleBufferedPanel: Panel
     {
         public DoubleBufferedPanel()
         {
-            this.DoubleBuffered = true;
+            this.DoubleBuffered = false;
         }
     }
 }
