51bda0457b40c1212b89b4ed1d7d38a6f604590c
Aldas Taraškevičius <aldas.taraskevicius@ktu.edu>
2019-11-14 19:11:52 +0200

    Misc improvements
    Cleaned up Connect method, made logger's capability of logging with
    color public

--- Server/Logger.cs
+++ Server/Logger.cs
@@ -16,21 +16,21 @@ namespace Server
         /// Logs a regular message to the console.
         /// </summary>
         /// <param name="msg">Message to log.</param>
-        public void LogMessage(string msg) => LogColor(ConsoleColor.White, msg);
+        public void LogMessage(string msg) => LogWithColor(ConsoleColor.White, msg);
 
         /// <summary>
         /// Logs a warning message to the console.
         /// </summary>
         /// <param name="msg">Warning message to log.</param>
-        public void LogWarning(string msg) => LogColor(ConsoleColor.Yellow, "[WARNING]: " + msg);
+        public void LogWarning(string msg) => LogWithColor(ConsoleColor.Yellow, "[WARNING]: " + msg);
 
         /// <summary>
         /// Logs an error message to the console.
         /// </summary>
         /// <param name="msg">Error message to log.</param>
-        public void LogError(string msg) => LogColor(ConsoleColor.Red, "[ERROR]: " + msg);
+        public void LogError(string msg) => LogWithColor(ConsoleColor.Red, "[ERROR]: " + msg);
 
-        private void LogColor(ConsoleColor textColor, string msg)
+        public void LogWithColor(ConsoleColor textColor, string msg)
         {
             Console.ForegroundColor = textColor;
             Console.WriteLine(msg);
