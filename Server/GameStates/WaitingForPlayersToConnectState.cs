e96474de447a0a4f84f9f85743c4231b8979536d
Aldas Taraškevičius <aldas.taraskevicius@ktu.edu>
2019-12-10 23:18:32 +0200

    Implemented memento pattern
    Implemented the memento pattern for game state storage. If all players
    leave during a game, the state of the game is saved. When at least one
    player joins, rather than starting a new game every time, the game is
    resumed using the restored state.

--- Server/GameStates/WaitingForPlayersToConnectState.cs
+++ Server/GameStates/WaitingForPlayersToConnectState.cs
@@ -18,7 +18,10 @@ namespace Server.GameStates
         {
             const string message = "[GAME STATE] No players connected: waiting for at least one to join.";
             Logger.Instance.LogWithColor(ConsoleColor.Blue, message);
-            _arena.Reset();
+            
+            // Reset arena only if there is no previous state.
+            if(_context.Caretaker.Memento == null)
+                _arena.Reset();
         }
 
         public void RunTick()
@@ -26,11 +29,24 @@ namespace Server.GameStates
             // Switch to pregame state if there are players connected, otherwise wait.
             if (!_arena.Players.Any())
                 return;
-
-            var pregameCountdownState = new PregameCountdownState(_arena, _context);
-            _context.ChangeState(pregameCountdownState);
+            
+            // If there is no memento in the caretaker, assume that a new game will start,
+            // otherwise, restore state.
+            if (_context.Caretaker.Memento == null)
+            {
+                var pregameCountdownState = new PregameCountdownState(_arena, _context);
+                _context.ChangeState(pregameCountdownState);
+            }
+            else
+            {
+                var previousStateMemento = _context.Caretaker.Memento;
+                _context.RestoreFromGameStateMemento(previousStateMemento);
+            }
         }
 
         public GameStateEnum ToEnum() => GameStateEnum.Pending;
+        
+        // This state doesn't really have a set duration.
+        public int GetRemainingDuration() => -1;
     }
 }
---
+++ Server/Memento/GameStateMemento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Server.GameStates;
+
+namespace Server.Memento
+{
+    /// <summary>
+    /// A memento object which stores a game state.
+    /// </summary>
+    public class GameStateMemento
+    {
+        /// <summary>
+        /// The game state stored in this memento.
+        /// </summary>
+        public IGameState StoredGameState { get; }
+
+        /// <summary>
+        /// Constructs a game state memento.
+        /// </summary>
+        /// <param name="state">Game state to store.</param>
+        public GameStateMemento(IGameState state)
+        {
+            StoredGameState =
+                state ?? throw new ArgumentNullException(nameof(state),
+                    "Null passed to game state memento constructor!");
+        }
+    }
+}
